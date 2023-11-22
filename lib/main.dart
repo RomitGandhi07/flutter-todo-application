1081abb9cc19c4df024b3ce461e9e84084186522
RomitGandhi07 <gandhiromit77@gmail.com>
2023-11-22 23:13:20 +0530

    Addded tasks TODO implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,179 +1,327 @@
-import 'dart:async';
-
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:toast/toast.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Parse().initialize(
+    'kCIAO9W4z7DGfzH0S81KWWEv0RaXNlIUQoOY3CrE',
+    'https://parseapi.back4app.com',
+    clientKey: 'M7xFLbCb7fFRtMusgR9xt5vjP3nFWJY7O9xhb4TM',
+    autoSendSessionId: true,
+    debug: true,
+  );
 
-  final keyApplicationId = 'kCIAO9W4z7DGfzH0S81KWWEv0RaXNlIUQoOY3CrE';
-  final keyClientKey = 'M7xFLbCb7fFRtMusgR9xt5vjP3nFWJY7O9xhb4TM';
-  final keyParseServerUrl = 'https://parseapi.back4app.com';
-
-  await Parse().initialize(keyApplicationId, keyParseServerUrl,
-      clientKey: keyClientKey, debug: true);
-
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(MyApp());
 }
 
-class Home extends StatefulWidget {
+class Task {
+  late String title;
+  late String description;
+
+  Task({required this.title, required this.description});
+}
+
+class MyApp extends StatelessWidget {
   @override
-  _HomeState createState() => _HomeState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Back4App Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: TodoList(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
-class _HomeState extends State<Home> {
-  final todoController = TextEditingController();
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
 
-  void addToDo() async {
-    if (todoController.text.trim().isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Empty title"),
-        duration: Duration(seconds: 2),
-      ));
-      return;
+class _TodoListState extends State<TodoList> {
+  late List<ParseObject> tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTasks();
+  }
+
+  _loadTasks() async {
+    QueryBuilder<ParseObject> queryBuilder =
+        QueryBuilder<ParseObject>(ParseObject('Task'))
+          ..orderByDescending('createdAt');
+    var apiResponse = await queryBuilder.query();
+    if (apiResponse.success && apiResponse.results != null) {
+      setState(() {
+        tasks = List<ParseObject>.from(apiResponse.results!);
+      });
     }
-    await saveTodo(todoController.text);
-    setState(() {
-      todoController.clear();
-    });
+  }
+
+  _addTask(String title, String description) async {
+    ParseObject newTask = ParseObject('Task')
+      ..set<String>('title', title)
+      ..set<String>('description', description);
+
+    var apiResponse = await newTask.save();
+    if (apiResponse.success) {
+      _loadTasks();
+      _showSnackBar("Task Added Successfully");
+      // _showToast("Task Added Successfully");
+      // _showToast(context, "Task Added Successfully");
+    } else {
+      _showErrorDialog(apiResponse.error!.message, context);
+    }
+  }
+
+  _updateTask(ParseObject task, String title, String description) async {
+    task.set<String>('title', title);
+    task.set<String>('description', description);
+
+    var apiResponse = await task.save();
+    if (apiResponse.success) {
+      // Reload tasks after updating a task
+      _loadTasks();
+      _showSnackBar("Task Updated Successfully");
+      // _showToast("Task Updated Successfully");
+      // _showToast(context, "Task Updated Successfully");
+    } else {
+      _showErrorDialog(apiResponse.error!.message, context);
+    }
+  }
+
+  _deleteTask(ParseObject task) async {
+    var apiResponse = await task.delete();
+    if (apiResponse.success) {
+      _loadTasks();
+      _showSnackBar("Task Deleted Successfully");
+      // _showSnackBar("Task Deleted Successfully");
+      // _showToast(context, "Task Deleted Successfully");
+    } else {
+      _showErrorDialog(apiResponse.error!.message, context);
+    }
+  }
+
+  _showErrorDialog(String errorMessage, BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(errorMessage),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /*void _showToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }*/
+
+  /*void _showToast(BuildContext context, String message) {
+    Toast.show(
+      message,
+      context,
+      duration: Toast.LENGTH_SHORT,
+      gravity: Toast.BOTTOM,
+    );
+  }*/
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+
+  // Future<void> _showAddTaskDialog(BuildContext context) async {
+  _showAddTaskDialog(BuildContext context) async {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController descriptionController = TextEditingController();
+
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add New Task'),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // Close the dialog
+            },
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () async {
+              await _addTask(titleController.text, descriptionController.text);
+              Navigator.pop(context); // Close the dialog
+            },
+            child: Text('Add Task'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _showEditTaskDialog(ParseObject task) async {
+    TextEditingController titleController =
+        TextEditingController(text: task.get('title'));
+    TextEditingController descriptionController =
+        TextEditingController(text: task.get('description'));
+
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Edit Task'),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // Close the dialog
+            },
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              _updateTask(
+                  task, titleController.text, descriptionController.text);
+              Navigator.pop(context); // Close the dialog
+            },
+            child: Text('Update Task'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _showDeleteTaskDialog(ParseObject task) async {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Confirm Delete'),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Are you sure you want to delete the task below?'),
+            SizedBox(height: 8.0),
+            Text(
+              task.get('title'),
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // Close the dialog
+            },
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              _deleteTask(task);
+              Navigator.pop(context); // Close the dialog
+            },
+            child: Text('Delete'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Parse Todo List"),
-        backgroundColor: Colors.blueAccent,
-        centerTitle: true,
+        title: Text('Task List'),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-              padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      autocorrect: true,
-                      textCapitalization: TextCapitalization.sentences,
-                      controller: todoController,
-                      decoration: InputDecoration(
-                          labelText: "New todo",
-                          labelStyle: TextStyle(color: Colors.blueAccent)),
-                    ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          ParseObject task = tasks[index];
+          return Card(
+            elevation: 4.0,
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: ListTile(
+              title: Text(task.get('title')),
+              subtitle: Text(task.get('description')),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.edit, color: Colors.blue),
+                    onPressed: () {
+                      _showEditTaskDialog(task);
+                    },
                   ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        onPrimary: Colors.white,
-                        primary: Colors.blueAccent,
-                      ),
-                      onPressed: addToDo,
-                      child: Text("ADD")),
+                  IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () {
+                      _showDeleteTaskDialog(task);
+                    },
+                  ),
                 ],
-              )),
-          Expanded(
-              child: FutureBuilder<List<ParseObject>>(
-                  future: getTodo(),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.none:
-                      case ConnectionState.waiting:
-                        return Center(
-                          child: Container(
-                              width: 100,
-                              height: 100,
-                              child: CircularProgressIndicator()),
-                        );
-                      default:
-                        if (snapshot.hasError) {
-                          return Center(
-                            child: Text("Error..."),
-                          );
-                        }
-                        if (!snapshot.hasData) {
-                          return Center(
-                            child: Text("No Data..."),
-                          );
-                        } else {
-                          return ListView.builder(
-                              padding: EdgeInsets.only(top: 10.0),
-                              itemCount: snapshot.data!.length,
-                              itemBuilder: (context, index) {
-                                //*************************************
-                                //Get Parse Object Values
-                                final varTodo = snapshot.data![index];
-                                final varTitle = '';
-                                final varDone = false;
-                                //*************************************
-
-                                return ListTile(
-                                  title: Text(varTitle),
-                                  leading: CircleAvatar(
-                                    child: Icon(
-                                        varDone ? Icons.check : Icons.error),
-                                    backgroundColor:
-                                        varDone ? Colors.green : Colors.blue,
-                                    foregroundColor: Colors.white,
-                                  ),
-                                  trailing: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Checkbox(
-                                          value: varDone,
-                                          onChanged: (value) async {
-                                            await updateTodo(
-                                                varTodo.objectId!, value!);
-                                            setState(() {
-                                              //Refresh UI
-                                            });
-                                          }),
-                                      IconButton(
-                                        icon: Icon(
-                                          Icons.delete,
-                                          color: Colors.blue,
-                                        ),
-                                        onPressed: () async {
-                                          await deleteTodo(varTodo.objectId!);
-                                          setState(() {
-                                            final snackBar = SnackBar(
-                                              content: Text("Todo deleted!"),
-                                              duration: Duration(seconds: 2),
-                                            );
-                                            ScaffoldMessenger.of(context)
-                                              ..removeCurrentSnackBar()
-                                              ..showSnackBar(snackBar);
-                                          });
-                                        },
-                                      )
-                                    ],
-                                  ),
-                                );
-                              });
-                        }
-                    }
-                  }))
-        ],
+              ),
+            ),
+          );
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddTaskDialog(context);
+        },
+        child: Icon(Icons.add),
+        elevation: 2.0,
+        // backgroundColor: Colors.blue,
+      ),
+      // floatingActionButton:CenterFabButton()
     );
-  }
-
-  Future<void> saveTodo(String title) async {
-    print(title);
-    await Future.delayed(Duration(seconds: 1), () {});
-  }
-
-  Future<List<ParseObject>> getTodo() async {
-    await Future.delayed(Duration(seconds: 2), () {});
-    return [];
-  }
-
-  Future<void> updateTodo(String id, bool done) async {
-    await Future.delayed(Duration(seconds: 1), () {});
-  }
-
-  Future<void> deleteTodo(String id) async {
-    await Future.delayed(Duration(seconds: 1), () {});
   }
 }
